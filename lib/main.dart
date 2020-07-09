cc1b3a638854752883365790f01bd51e04ea089d
Smarth <smarth_bansal@yahoo.com>
2020-07-10 00:45:07 +0530

    Refactored Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sih2020/screens/registration_screen.dart';
-import 'welcome_screen.dart';
+import 'screens/welcome_screen.dart';
 import 'screens/login_screen.dart';
 
 void main() {
